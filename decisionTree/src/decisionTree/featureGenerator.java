0f5fb31c2d9f1a9ce658f5f90728cfeadda86972
Rafael Angelo <rlangelo@wpi.edu>
2015-02-07 16:51:46 -0500

    reading input files

--- decisionTree/src/decisionTree/featureGenerator.java
+++ decisionTree/src/decisionTree/featureGenerator.java
@@ -1,5 +1,38 @@
 package decisionTree;
 
+import java.io.*;
+
+
+
 public class featureGenerator {
 
+	File inputFile;
+	File outputFile;
+	
+	public featureGenerator()
+	{
+
+	}
+	
+	public void generate(String fileName) throws FileNotFoundException, IOException
+	{
+		inputFile = new File("./" + fileName);
+		BufferedReader br = new BufferedReader(new FileReader(inputFile));
+		
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			System.out.println(line);
+		}
+		
+		br.close();
+	}
+	
+	public static void main(String [] arg) throws IOException
+	{
+		String fileName = arg[0];
+		featureGenerator fg = new featureGenerator();
+		
+		fg.generate(fileName);
+	}
+	
 }
