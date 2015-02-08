b0b3fc58ed8784bf5e3fcf82331c70f5ba071b7f
Rafael Angelo <rlangelo@wpi.edu>
2015-02-07 19:35:13 -0500

    Hopefully it will come back.

---
+++ decisionTree/src/decisionTree/checkCenter.java
@@ -0,0 +1,20 @@
+package decisionTree;
+
+public class checkCenter {
+
+	public int totalCenter(int[][] board, int player, int width, int height)
+	{
+		int total = 0;
+		for (int i=width/2-1; i<=width/2+1;i++)
+		{
+			for (int j=0; j<height; j++)
+			{
+				if (board[i][j] == player)
+				{
+					total++;
+				}
+			}
+		}
+		return total;
+	}
+}
