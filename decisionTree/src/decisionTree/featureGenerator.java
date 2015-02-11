b5e6f50a251432e8e208f3b42b6e509a9e09367a
djbenson29 <djbenson@wpi.edu>
2015-02-10 19:52:10 -0500

    adds multiplication features and corrects output

--- decisionTree/src/decisionTree/featureGenerator.java
+++ decisionTree/src/decisionTree/featureGenerator.java
@@ -22,6 +22,8 @@ public class featureGenerator {
 		inputFile = new File("./" + fileName);
 		BufferedReader br = new BufferedReader(new FileReader(inputFile));
 		int i = 0;
+		int featureList[] = new int[30];
+		int counter = 5;
 		
 		String line = null;
 		int[][] board = new int[100][100];
@@ -34,21 +36,34 @@ public class featureGenerator {
 			checkCenter cCenter = new checkCenter();
 			checkFullBoard cFull = new checkFullBoard();
 			checkOpposingTwos coTwo = new checkOpposingTwos();
-			int feature1 = cTwo.totalTwo(board, 7, 6, 1);
-			int feature2 = cThree.totalThree(board, 7, 6, 1);
-			int feature3 = cCenter.totalCenter(board, 1, 7, 6);
-			int feature4 = cFull.foursBoardFull(board, 1);
-			int feature5 = coTwo.totalTwo(board, 7, 6, 1);
-			System.out.println(feature1 + " ");
-			System.out.println(feature2 + " ");
-			System.out.println(feature3 + " ");
-			System.out.println(feature4 + " ");
-			System.out.println(feature5 + " ");
+			featureList[0] = cTwo.totalTwo(board, 7, 6, 1);
+			featureList[1] = cThree.totalThree(board, 7, 6, 1);
+			featureList[2] = cCenter.totalCenter(board, 1, 7, 6);
+			featureList[3] = cFull.foursBoardFull(board, 1);
+			featureList[4] = coTwo.totalTwo(board, 7, 6, 1);
+			System.out.println(featureList[0] + " ");
+			System.out.println(featureList[1] + " ");
+			System.out.println(featureList[2] + " ");
+			System.out.println(featureList[3] + " ");
+			System.out.println(featureList[4] + " ");
+			
+			for (int x=0; x<5; x++) {
+				for (int y=0; y<5; y++) {
+					int q = featureList[x];
+					int p = featureList[y];
+					featureList[counter] = q*p;
+					System.out.println("featureList[counter] = " + featureList[counter] + ". FeatureList[x] = "+ featureList[x] + " y = " + featureList[y]);
+					counter++;
+				}
+			}
+			
+			counter = 5;
+			
 			if (i==0){
-				deconstructBoard("output.csv", board, feature1, feature2, feature3, feature4, feature5, winner, true);
+				deconstructBoard("output.csv", board, featureList, winner, true);
 			}
 			else{
-				deconstructBoard("output.csv", board, feature1, feature2, feature3, feature4, feature5, winner, false);
+				deconstructBoard("output.csv", board, featureList, winner, false);
 			}
 			System.out.println("\n");
 			i++;
@@ -58,8 +73,7 @@ public class featureGenerator {
 	}
 	
 	// Turns a board back into a CSV readable file
-	public void deconstructBoard(String fileName, int[][] board, int feature1, int feature2, int feature3, int feature4, 
-			int feature5, String winner, boolean first) throws FileNotFoundException, IOException {
+	public void deconstructBoard(String fileName, int[][] board, int features[], String winner, boolean first) throws FileNotFoundException, IOException {
 		outputFile = new File ("./" + fileName);
 		if (first){
 			if (outputFile.exists()){
@@ -72,14 +86,20 @@ public class featureGenerator {
 		BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile.getAbsoluteFile(), true));
 		if (first){
 			bw.write("s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18,s19,s20,s21,s22,s23,s24,s25,s26,s27,s28,"
-					+ "s29,s30,s31,s32,s33,s34,s35,s36,s37,s38,s39,s40,s41,s42,f1,f2,f3,f4,f5,winner\n");
+					+ "s29,s30,s31,s32,s33,s34,s35,s36,s37,s38,s39,s40,s41,s42,f1,f2,f3,f4,f5,f1*f1,f1*f2,f1*f3,f1*f4,f1*f5,"
+					+ "f2*f1,f2*f2,f2*f3,f2*f4,f2*f5,f3*f1,f3*f2,f3*f3,f3*f4,f3*f5,f4*f1,f4*f2,f4*f3,f4*f4,f4*f5,"
+					+ "f5*f1,f5*f2,f5*f3,f5*f4,f5*f5,winner\n");
 		}
 		for (int i=0; i<7; i++){
 			for (int j=0; j<6; j++){
 				bw.write(board[j][i] + ",");
 			}
 		}
-		bw.write(feature1 + "," + feature2 + "," + feature3 + "," + feature4 + "," + feature5 + "," + winner);
+		
+		for (int i=0; i<29; i++){
+			bw.write(features[i] + ",");
+		}
+		bw.write(winner);
 		bw.write("\n");
 		bw.close();
 	}
